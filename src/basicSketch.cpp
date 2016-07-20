9b680d4a12961d1d0c8c0cc3049213e6ff9c9313
Nidal P <nidalp@arduino-xc.com>
2016-07-20 18:29:03 -0500

    time lapse function that takes into account timer/counter wrap around.

--- src/basicSketch.cpp
+++ src/basicSketch.cpp
@@ -15,15 +15,48 @@ void setup(void)
 int ledState = LOW;
 int time = 0;
 
+int timeStart = millis();
+#define MAX_MILLIS 477218
+
+bool timeLapseMillis(int deltaMs)
+{
+    int now;
+    int delta = 0;
+
+        now = millis();
+        if(now < timeStart)
+        {
+            // this handles timer counter overflow
+            // when counter overflows, 'now' becomes < 'start'
+            delta = MAX_MILLIS - timeStart + now;
+        }
+        else
+        {
+            // regular situation is when 'start' comes before 'now'
+            delta = now - timeStart;
+        }
+
+        if(delta < deltaMs)
+            return false;
+
+        timeStart = now;
+        return true;
+}
+
 void loop(void)
 {
-    int now = millis();
-    if(now - time > 1000)
+    if(timeLapseMillis(1000))
 	{
-    	time = now;
 		digitalWrite(LED_BUILTIN, ledState);
 		ledState = ledState == LOW ? HIGH : LOW;
 	}
+//    int now = millis();
+//    if(now - time > 1000)
+//	{
+//    	time = now;
+//		digitalWrite(LED_BUILTIN, ledState);
+//		ledState = ledState == LOW ? HIGH : LOW;
+//	}
 
     yield();
 }
