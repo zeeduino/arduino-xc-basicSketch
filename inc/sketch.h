eb9eb28dc99321fb4b4ae8a9380e9302aa03efa0
Nidal P <nidalp@arduino-xc.com>
2016-08-18 19:46:58 -0500

    refactored some code and added banner printer.

---
+++ inc/sketch.h
@@ -0,0 +1,17 @@
+/*
+ * sketch.h
+ *
+ *  Created on: Aug 18, 2016
+ *      Author: iceman
+ */
+
+#ifndef SKETCH_H_
+#define SKETCH_H_
+
+
+void printBanner(void);
+bool timeLapseMillis(int deltaMs);
+
+
+
+#endif /* SKETCH_H_ */
--- src/basicSketch.cpp
+++ src/basicSketch.cpp
@@ -1,47 +1,23 @@
 #include "Arduino.h"
+#include "sketch.h"
 
 #define FW_VERSION "0.1.0"
+
 
 void setup(void)
 {
     Serial.begin(115200, SERIAL_8N1);
-    Serial.println("app name");
-    Serial.print("v");
+    printBanner();
+
+    Serial.println("zeeduino basic sketch");
+    Serial.print("firmware v");
     Serial.println(FW_VERSION);
 
     pinMode(LED_BUILTIN, OUTPUT);
 }
 
 int ledState = LOW;
-int time = 0;
-
-int timeStart = millis();
-#define MAX_MILLIS 477218
-
-bool timeLapseMillis(int deltaMs)
-{
-    int now;
-    int delta = 0;
-
-        now = millis();
-        if(now < timeStart)
-        {
-            // this handles timer counter overflow
-            // when counter overflows, 'now' becomes < 'start'
-            delta = MAX_MILLIS - timeStart + now;
-        }
-        else
-        {
-            // regular situation is when 'start' comes before 'now'
-            delta = now - timeStart;
-        }
-
-        if(delta < deltaMs)
-            return false;
-
-        timeStart = now;
-        return true;
-}
+byte byteRead;
 
 void loop(void)
 {
@@ -50,13 +26,6 @@ void loop(void)
 		digitalWrite(LED_BUILTIN, ledState);
 		ledState = ledState == LOW ? HIGH : LOW;
 	}
-//    int now = millis();
-//    if(now - time > 1000)
-//	{
-//    	time = now;
-//		digitalWrite(LED_BUILTIN, ledState);
-//		ledState = ledState == LOW ? HIGH : LOW;
-//	}
 
     yield();
 }
